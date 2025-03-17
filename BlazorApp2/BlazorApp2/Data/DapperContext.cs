332233220c0eebc5e8de5c02fd05611f133ca40c
unknown <moazzemh222@gmail.com>
2025-03-18 00:23:33 +0600

    Added BlazorApp2

---
+++ BlazorApp2/BlazorApp2/Data/DapperContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BlazorApp2.Data
+{
+    public class DapperContext
+    {
+        private string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+    }
+}
